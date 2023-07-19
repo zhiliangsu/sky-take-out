90fca84f476fc9817ad7fb3ffeb4fac7896cffd7
zhiliangsu <zhiliangsuzhiliang@gmail.com>
2023-07-19 15:22:25 +0800

    add feature: employee management ==> page query

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -2,12 +2,15 @@ package com.sky.service;
 
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
 
 public interface EmployeeService {
 
     /**
      * 员工登录
+     *
      * @param employeeLoginDTO
      * @return
      */
@@ -20,4 +23,12 @@ public interface EmployeeService {
      * @return
      */
     void save(EmployeeDTO employeeDTO);
+
+    /**
+     * 员工列表分页查询
+     *
+     * @param employeePageQueryDTO
+     * @return
+     */
+    PageResult page(EmployeePageQueryDTO employeePageQueryDTO);
 }
