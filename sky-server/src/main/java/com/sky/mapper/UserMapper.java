1b351bb4c98f3bbda5f645c96d3fd652830fd36c
zhiliangsu <zhiliangsuzhiliang@gmail.com>
2023-07-28 10:13:02 +0800

    import code: admin -> workspace management

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -4,6 +4,8 @@ import com.sky.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.Map;
+
 @Mapper
 public interface UserMapper {
 
@@ -31,4 +33,11 @@ public interface UserMapper {
      */
     @Select("select * from user where id=#{id}")
     User getById(Long id);
+
+    /**
+     * 根据条件统计用户数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
